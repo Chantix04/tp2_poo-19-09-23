efd321d284122c052924501c69d3359279d5f7fa
Chantix04 <gstnrcld@gmail.com>
2023-09-27 14:38:54 -0300

    a

--- backend/src/productos/productos.controller.ts
+++ backend/src/productos/productos.controller.ts
@@ -22,5 +22,24 @@ export class Tienda {
         return this.productos
     }
 
+    addStock (id:number) {
+        this.productos.filter(productos => productos.getProducto().id === id)[0].aumentarStock(1)
+        return console.log('El stock del producto fue aumentado en 1')
+    }
+
+    addStockByNumber (id:number, stock:number) {
+        this.productos.filter(productos => productos.getProducto().id === id)[0].aumentarStock(stock)
+        return console.log(`El stock del producto fue aumentado en ${stock}`)
+    }
+
+    decreaseStock (id:number) {
+        this.productos.filter(productos => productos.getProducto().id === id)[0].descontarStock(1)
+        return console.log(`El stock del producto disminuyó en 1`)
+    }
+
+    decreaseStockByNumber (id: number, stock:number) {
+        this.productos.filter(productos => productos.getProducto().id === id)[0].descontarStock(stock)
+        return console.log(`El stock del producto fue disminuido en ${stock}`)
+    }
     
 }
